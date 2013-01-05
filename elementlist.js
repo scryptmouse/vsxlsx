c248f30ae6419e2d1b4f192eb35a795d319dfd84
Alexa Grey <devel@mouse.vc>
2013-01-05 04:43:08 -0800

    Initial documentation commit.

---
+++ elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["f","alphacize()"],["f","column_index()"],["f","column_name()"],["f","default_columns()"],["f","indicize()"],["f","recursive_rm()"],["f","row_index()"],["c","SimpleXMLElement"],["c","Traversable"],["c","VSXLSX\\Parser"]];
